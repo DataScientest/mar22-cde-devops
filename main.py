1d6ba33bebd6610cf834baf372e3e03431ae0ed4
pauldechorgnat <pdechorgnat@yahoo.fr>
2022-07-21 13:08:44 +0200

    Ajout fichier main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+
+api = FastAPI(title="Mon API pour les MAR22 CDE")
+
+# GET /hello
+# -> {"message": "hello!"}
+
+
+@api.get(
+    "/hello",
+    tags=["hello", "world"],
+    responses={
+        200: {
+            "description": "Tout va bien dans le meilleur des mondes!"
+        }
+    })
+def get_hello():
+    """Une fonction qui salue bien les gens qui utilisent cette API!
+
+    Returns:
+        dict: Message de salutations
+    """
+    return {
+        "message": "hello"
+    }
+
+
+@api.get("/health")
+def get_health():
+    return {
+        "status": 1
+    }
